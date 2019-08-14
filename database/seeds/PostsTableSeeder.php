f4574f36058df5df2f3c127366b7494f1fcd781c
Makara Chhin <cmpsnl310320@gmail.com>
2019-08-15 06:51:53 +0700

    Add view_count column to posts table and Display popular posts and fix thumbnail

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -31,15 +31,16 @@ class PostsTableSeeder extends Seeder
           $createdDate = clone($date);
 
           $posts[] = [
-              'author_id' => rand(1, 3),
-              'title' => $faker->sentence(rand(8, 12)),
-              'excerpt' => $faker->text(rand(250, 300)),
-              'body' => $faker->paragraphs(rand(10, 15), true),
-              'slug' => $faker->slug(),
-              'image' => rand(0, 1) == 1 ? $image : NULL,
-              'created_at' => $createdDate,
+            'author_id' => rand(1, 3),
+            'title' => $faker->sentence(rand(8, 12)),
+            'excerpt' => $faker->text(rand(250, 300)),
+            'body' => $faker->paragraphs(rand(10, 15), true),
+            'slug' => $faker->slug(),
+            'image' => rand(0, 1) == 1 ? $image : NULL,
+            'created_at' => $createdDate,
         		'updated_at' => $createdDate,
-            'published_at' => $i < 5 ? $publishedDate : (rand(0, 1) == 0 ? NULL : $publishedDate->addDays(4) )
+            'published_at' => $i < 5 ? $publishedDate : (rand(0, 1) == 0 ? NULL : $publishedDate->addDays(4) ),
+            'view_count' => rand(1, 10) * 10
 
           ];
       }
