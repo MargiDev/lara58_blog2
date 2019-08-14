c0a0ac3c431c4caab03c5b7fa39613eddc73ec7e
Makara Chhin <cmpsnl310320@gmail.com>
2019-08-14 16:04:13 +0700

    Setup application layout and create post table with migration

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MyBlog | My Awesome Blog</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/custom.css">
+</head>
+<body>
+
+    @include('layouts.header')
+
+    @yield('content')
+
+    @include('layouts.footer')
+
+<script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
