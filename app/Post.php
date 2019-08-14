7b47e467930c9c35f43be3d8af2db8f75a533dac
Makara Chhin <cmpsnl310320@gmail.com>
2019-08-14 17:14:53 +0700

    Display user post and created post in the excerpt and convert all post to laravel pagination

--- app/Post.php
+++ app/Post.php
@@ -6,16 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function getImageUrlAttribute($value)
-    {
-      $imageUrl = "";
-      if( ! is_null($this->image)){
-        $imagePath = public_path() .'/img/'.$this->image;
-        if(file_exists($imagePath)){
-          $imageUrl = asset('img/'. $this->image);
-        }
-      }
+  public function author()
+  {
+    return $this->belongsTo(User::class);
+  }
 
-      return $imageUrl;
+  public function getImageUrlAttribute($value)
+  {
+    $imageUrl = "";
+    if( ! is_null($this->image)){
+      $imagePath = public_path() .'/img/'.$this->image;
+      if(file_exists($imagePath)){
+        $imageUrl = asset('img/'. $this->image);
+      }
     }
+
+    return $imageUrl;
+  }
+
+  public function getDateAttribute($value)
+  {
+    return $this->created_at->diffForHumans();
+  }
+
+  public function scopeLatestFirst($query)
+  {
+    return $query->orderBy('created_at', 'desc');
+  }
 }
