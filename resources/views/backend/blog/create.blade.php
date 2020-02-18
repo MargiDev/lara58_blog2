b60f413d750faa7c48db6d1af7cdc6f4d24f68b0
MargiDev <cmpsnl310320@gmail.com>
2020-02-18 09:53:31 +0700

    Generate slug automatically

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -110,3 +110,26 @@
   <!-- /.content -->
 </div>
 @endsection
+
+@section('script')
+
+  <script type="text/javascript">
+
+    $('ul.pagination').addClass('no-margin pagination-sm');
+
+    $('#title').on('blur', function(){
+      var theTitle = this.value.toLowerCase().trim(),
+          slugInput = $('#slug'),
+          theSlug = theTitle.replace(/&/g, '-and-') // replace & with -and-
+                            .replace(/[^a-z0-9-]+/g, '-') // replace empty space with -
+                            .replace(/\-\-+/g, '-') // replace more than one - or + with only one -
+                            .replace(/^-+|-+$/g, ''); // + or - on the left and right should be remove replace with empty space
+
+
+
+      slugInput.val(theSlug);
+    });
+
+  </script>
+
+@endsection
